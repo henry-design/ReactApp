afcf9b1d4779e7cd82a2627e2a28f533ea972873
Henry <henrymacharia09@gmail.com>
2022-04-19 20:13:46 +0300

    Table complete

--- src/components/MainDash/MainDash.jsx
+++ src/components/MainDash/MainDash.jsx
@@ -1,13 +1,13 @@
 import React from 'react';
 import Cards from '../Cards/Cards';
-import Table from '../Table/Table';
+import BasicTable from '../BasicTable/BasicTable';
 import './MainDash.css'
 function MainDash() {
   return (
-    <div class="MainDash">      
+    <div className="MainDash">      
       <h1>Dashboard</h1>
       <Cards/>
-      <Table/>
+      <BasicTable/>
     </div>
   );
 }
